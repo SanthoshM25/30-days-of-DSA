e7fd9159f83ebc271a30affd0029b1fa69b0a55d
SanthoshM25 <54148035+SanthoshM25@users.noreply.github.com>
2021-08-05 16:55:02 +0530

    Day 5 solution

--- Day 5/moveZerosBruteForce.java
+++ Day 5/moveZerosBruteForce.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Solution {
     public void moveZeroes(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
@@ -8,5 +10,22 @@ class Solution {
                 }
             }
         }
+        for (int j = 0; j < nums.length; j++) {
+            System.out.print(nums[j] + " ");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter no of digits: ");
+        int n = inp.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = inp.nextInt();
+        }
+        Solution s = new Solution();
+        s.moveZeroes(nums);
     }
 }
--- Day 5/moveZerosOptimal.java
+++ Day 5/moveZerosOptimal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int nonZero = 0, temp = 0;
@@ -8,5 +10,22 @@ class Solution {
                 nums[i] = temp;
             }
         }
+        for (int j = 0; j < nums.length; j++) {
+            System.out.print(nums[j] + " ");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter no of digits: ");
+        int n = inp.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = inp.nextInt();
+        }
+        Solution s = new Solution();
+        s.moveZeroes(nums);
     }
 }
