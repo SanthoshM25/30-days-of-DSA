e38926f13accf48a040bc973359574c745fdc8f9
SanthoshM25 <54148035+SanthoshM25@users.noreply.github.com>
2021-08-27 16:43:00 +0530

    Day 26 solution

---
+++ Day 26/Main.java
@@ -0,0 +1,11 @@
+class GfG {
+    // Function to reverse the queue.
+    public Queue<Integer> rev(Queue<Integer> q) {
+        Stack<Integer> stack = new Stack<>();
+        while (q.size() > 0)
+            stack.push(q.remove());
+        while (!stack.empty())
+            q.add(stack.pop());
+        return q;
+    }
+}
